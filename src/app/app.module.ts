15823796a7c128ebf193812a69c465e7ab11cbad
CesarAbraham0428 <CesarAbraham0428@gmail.com>
2024-06-12 20:25:37 -0600

    Agregando Mejoras Inicio  desde Cesar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from "./pages/inicio/inicio.component";
+import { PredeterminadoComponent } from './pages/paquetes/predeterminado/predeterminado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    PredeterminadoComponent
   ],
   imports: [
     BrowserModule,

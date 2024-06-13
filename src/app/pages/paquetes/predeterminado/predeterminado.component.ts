077ebdf76f99f1f6d7b64ddb9e7a0fae983f1ba9
CesarAbraham0428 <CesarAbraham0428@gmail.com>
2024-06-12 19:53:55 -0600

    Estructura inicial del Proyecto desde Main

---
+++ src/app/pages/paquetes/predeterminado/predeterminado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-predeterminado',
+  templateUrl: './predeterminado.component.html',
+  styleUrl: './predeterminado.component.scss'
+})
+export class PredeterminadoComponent {
+
+}

44f93ba3a653db724c16ec29c34b85a88ad0bc3c
CesarAbraham0428 <lopezcesar0428@gmail.com>
2024-06-20 23:09:54 -0600

    Agregando menu responsive v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'agencia-dh';
+  menuActive: boolean = false;
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+  }
 }

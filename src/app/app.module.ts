6292cdfd9236de538ffc85567b6b6fe3c50ced55
Marco33100 <martinezramirezm82@gmail.com>
2024-07-01 09:03:41 -0600

    Agragar los contactso de la agencia que hizo gael y modificando registro, ademas que se autentica con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { LoginComponent } from './pages/login/login.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import firebase from 'firebase/compat/app';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -20,15 +23,15 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-  
-
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration(),
@@ -36,7 +39,7 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
     provideFirebaseApp(() => initializeApp({"projectId":"agencia-dh","appId":"1:852934717287:web:a3cb9ed8df20dd4fd6c9c2","databaseURL":"https://agencia-dh-default-rtdb.firebaseio.com","storageBucket":"agencia-dh.appspot.com","apiKey":"AIzaSyCkZ_mgpJnvZ2OgbxJw46QJVEA5pGjogR0","authDomain":"agencia-dh.firebaseapp.com","messagingSenderId":"852934717287","measurementId":"G-MX15MM3G5W"})),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth())
-  
+
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,19 +1,41 @@
-import { Component } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../user.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss'
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit{
 
+  formReg: FormGroup;
 
-  constructor (private router:Router) {}
+  constructor (
+    private UserService: UserService,
+    private router:Router
+  ){
+    this.formReg = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('')
+
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.UserService.register(this.formReg.value)
+    .then(response => {
+      console.log(response);
+    })
+  }
 
   iniciarSesion(){
     this.router.navigate([''])
   }
-
 
 }

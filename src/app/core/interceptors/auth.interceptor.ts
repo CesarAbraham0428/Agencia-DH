4efcc318bca07cbf2c081f4545ba5de4c8dab7e9
GalletitaQR <153694033+GalletitaQR@users.noreply.github.com>
2024-07-27 00:54:20 -0600

    El interceptor ahora envía al backend el token para verificación en Authorization (usando headers)

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,5 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const authToken = localStorage.getItem('authToken');
+
+  if (authToken) {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authToken}`
+      }
+    });
+    console.log('Cloned Request Headers:', clonedRequest.headers.get('Authorization')); // Log para verificar el encabezado
+    return next(clonedRequest);
+  } else {
+    console.log('No auth token found in localStorage'); // Log para verificar que el token está presente
+    return next(req);
+  }
 };

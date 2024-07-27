487d7c55c501da950c8df72a2e350fa8bd71e3f1
GalletitaQR <153694033+GalletitaQR@users.noreply.github.com>
2024-07-27 00:56:04 -0600

    Se quitaron logs de auth.interceptor.ts

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -9,10 +9,8 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
         Authorization: `Bearer ${authToken}`
       }
     });
-    console.log('Cloned Request Headers:', clonedRequest.headers.get('Authorization')); // Log para verificar el encabezado
     return next(clonedRequest);
   } else {
-    console.log('No auth token found in localStorage'); // Log para verificar que el token está presente
     return next(req);
   }
 };

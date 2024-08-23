4070dc6d3cffb82992e03ca0b1db8527d8037b09
CesarAbraham0428 <lopezcesar0428@gmail.com>
2024-08-22 21:17:03 -0600

    Prueba 8  cambiando variables de entorno de nuevo

--- src/app/environments/environments.prod.ts
+++ src/app/environments/environments.prod.ts
@@ -1,9 +1,14 @@
-//produccion
+//pruebas
 /*export const environment = {
   baseUrl: 'http://agencia-viajes'
 } */
 
-  export const environment = {
+/*   export const environment = {
     production: true,
     baseUrl: 'https://backenddhagencias-production-f839.up.railway.app'
-  }
+  } */
+
+    export const environment = {
+      production: true,
+      baseUrl: '${process.env.BACKEND_URL}'
+    };
--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,8 +1,15 @@
-//pruebas
+//Produccion
 /* export const environment = {
   baseUrl: 'http://agencia-viajes'
 } */
-export const environment = {
-  production: false,
+
+/* export const environment = {
+  production: true,
   baseUrl: 'https://backenddhagencias-production-f839.up.railway.app'
 }
+ */
+
+export const environment = {
+  production: true,
+  baseUrl: '${process.env.BACKEND_URL}'
+};

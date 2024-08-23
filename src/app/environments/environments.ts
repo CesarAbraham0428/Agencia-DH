c017f905178f1c6a5a8c0b555fc096aba048638d
CesarAbraham0428 <lopezcesar0428@gmail.com>
2024-08-22 21:27:06 -0600

    Esta es la unica forma en las varibles de entorno que funciona el Backend

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -3,13 +3,8 @@
   baseUrl: 'http://agencia-viajes'
 } */
 
-/* export const environment = {
+export const environment = {
   production: true,
   baseUrl: 'https://backenddhagencias-production-f839.up.railway.app'
 }
- */
-
-export const environment = {
-  production: true,
-  baseUrl: '${process.env.BACKEND_URL}'
-};
+ 
